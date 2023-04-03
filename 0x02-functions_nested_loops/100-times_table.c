0ece7771c9e798b6f5acfb67ab5fbabf119d1a8b
Ndatimana assa <arthur.ndatimana22@gmail.com>
2023-04-03 15:21:08 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,67 +1,50 @@
 #include "main.h"
 
 
-/**
- * print_thousand - print out the digit in the thousand spot
- * @sum: Value to scan for thousand spot
- *
- * Return: Void
- */
-static void print_thousand(int sum)
-{
-	if (sum / 1000 == 0)
+	/**
+	 * print_times_table - prints the n times table, starting with 0
+	 * @n: number of the times table
+	 */
+	void print_times_table(int n)
 	{
-		_putchar(32);
-	}
-	else
-	{
-		_putchar('0' + (sum / 1000));
-	}
-}
+		int i, j, k;
 
 
-/**
- * print_hundred - print out the digit in the hundred spot
- * @sum: Value to scan for hundred spot
- *
- * Return: Void
- */
-static void print_hundred(int sum)
-{
-	if (sum / 100 == 0)
-	{
-		_putchar(32);
-	}
-	else
-	{
-		_putchar('0' + (sum / 100));
-	}
-}
-
-
-/**
- * print_tenth - print out the digit in the tenth spot
- * @sum: Value to scan for tenth spot
- *
- * Return: Void
- */
-static void print_tenth(int sum)
-{
-	if (sum / 10 == 0)
-	{
-		_putchar(32);
-	}
-	else
-	{
-		if ((sum / 10) > 9)
+		if (n >= 0 && n <= 15)
 		{
-			_putchar('0' + (sum / 10 % 10));
-		}
-		else
-		{
-			_putchar('0' + (sum / 10));
+			for (i = 0; i <= n; i++)
+			{
+				for (j = 0; j <= n; j++)
+				{
+					k = j * i;
+					if (j == 0)
+					{
+						_putchar(k + '0');
+					} else if (k < 10 && j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(k + '0');
+					} else if (k >= 10 && k < 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
+					} else if (k >= 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar((k / 100) + '0');
+						_putchar(((k / 10) % 10) + '0');
+						_putchar((k % 10) + '0');
+					}
+				}
+				_putchar('\n');
+			}
 		}
 	}
-}
-
 
